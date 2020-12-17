26996a07b0bf49a3e182e73a7a270b3638e184d1
h1431532403240 <h1431532403240@gmail.com>
2020-12-17 22:23:42 +0800

    時間輸入數值正負互換，使輸入符合人性，並加上註解。

--- getTime.py
+++ getTime.py
@@ -1,6 +1,7 @@
 import datetime
 
+# 輸入整數取得日期，如今日為12/12，輸入-1則會回傳12/11
 def times(day):
-    dayAgo = datetime.datetime.now() - datetime.timedelta(days = day)
+    dayAgo = datetime.datetime.now() + datetime.timedelta(days = day)
     outPut = dayAgo.strftime("%m/%d")
     return outPut
