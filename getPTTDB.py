59dda354a4dc02bad03a569844295078c1f66814
h1431532403240 <h1431532403240@gmail.com>
2020-12-18 00:16:48 +0800

    新增抓取日期、多抓取上一頁資料。

--- getPTTDB.py
+++ getPTTDB.py
@@ -1,26 +1,70 @@
 import requests
+import urllib.parse
+from requests_html import HTML
 from bs4 import BeautifulSoup
 
-#將網頁資料GET下來
-read = requests.get("https://www.ptt.cc/bbs/Lifeismoney/index.html").text
-read = read[:read.find('<div class="r-list-sep"></div>')]
+def get_ptt_data():
+    # 將網頁資料GET下來
+    url = 'https://www.ptt.cc/bbs/Lifeismoney/index.html'
+    read = HTML(html=requests.get(url).text)
+    controls = read.find('.action-bar a.btn.wide')
+    link = controls[1].attrs.get('href')
+    page_url = urllib.parse.urljoin('https://www.ptt.cc/', link)
+    get_main_data(url)
+    get_main_data(page_url)
 
-#將網頁資料以html.parser
-soup = BeautifulSoup(read,"html.parser")
+def get_main_data(url):
+    # 將網頁資料GET下來
+    read = requests.get(url).text
+    read = read[:read.find('<div class="r-list-sep"></div>')]
 
-#取HTML標中的 <div class="title"></div> 中的<a>標籤存入sel
-sel = soup.select("div.r-ent")
+    # 將網頁資料以html.parser
+    soup = BeautifulSoup(read,'html.parser')
 
-#取出超連結
-for s in sel:
-    #取得文章連結
-    title = s.select("div.title a")
-    href = title[0].get("href")
-    print(href)
-    #取得推文數
-    push = s.select("div.nrec span")
-    if push:
-        nrec = push[0].text
-    else:
-        nrec = 0
-    print(nrec)
+    # 取HTML標中的 <div class="title"></div> 中的<a>標籤存入sel
+    sel = soup.select('div.r-ent')
+
+    # 取出超連結
+    for s in sel:
+        # 取得文章連結
+        title = s.select('div.title a')
+        if title:
+            href = title[0].get('href')
+        else:
+            continue
+        print(href)
+        # 取得推文數
+        push = s.select('div.nrec span')
+        if push:
+            if push == '爆':
+                nrec = 100
+            elif push == 'X1':
+                nrec = -10
+            elif push == 'X2':
+                nrec = -20
+            elif push == 'X3':
+                nrec = -30
+            elif push == 'X4':
+                nrec = -40
+            elif push == 'X5':
+                nrec = -50
+            elif push == 'X6':
+                nrec = -60
+            elif push == 'X7':
+                nrec = -70
+            elif push == 'X8':
+                nrec = -80
+            elif push == 'X9':
+                nrec = -90
+            elif push == 'XX':
+                nrec = -100
+            else:
+                nrec = push[0].text
+        else:
+            nrec = 0
+        print(nrec)
+        # 取得文章日期
+        date = s.select('div.date')
+        print(date[0].string)
+
+get_ptt_data()
